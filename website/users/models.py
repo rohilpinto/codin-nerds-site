28da61be46116639a9bd9fed21413611e65d5077
janaSunrise <warriordefenderz@gmail.com>
2020-08-13 22:51:16 +0530

    work partially on user profile

--- website/users/models.py
+++ website/users/models.py
@@ -1,3 +1,12 @@
-# from django.db import models
+from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Learner(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=150, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    # points =
+
+    def __str__(self):
+        return self.name
