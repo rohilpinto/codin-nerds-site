0fb34ece3a53951e17bb3b0b76e3de0ce2d41d18
janaSunrise <warriordefenderz@gmail.com>
2020-08-14 23:08:29 +0530

    add blog adding feature

--- website/blog/urls.py
+++ website/blog/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.posts, name='blog-main'),
-    path('post/<str:pk>/', views.post, name='blog-post')
+    path('post/<str:pk>/', views.post, name='blog-post'),
+
+    path('create_post/', views.create_post, name='create_post')
 ]
--- website/blog/views.py
+++ website/blog/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+
 from .models import Post
+from .forms import PostForm
 
 
 def posts(request):
@@ -18,3 +21,19 @@ def post(request, pk):
         'post': post,
     }
     return render(request, 'blog/post.html', context)
+
+
+@login_required(login_url="login")
+def create_post(request):
+    form = PostForm()
+
+    if request.method == "POST":
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+        return redirect('blog-main')
+
+    context = {
+        'form': form,
+    }
+    return render(request, 'blog/post_form.html', context)
